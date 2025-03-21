39aba3eb2c34742d5e9f98c1506a45195be6f28e
cmatos <cmatos@uolinc.com>
2025-03-21 18:40:58 -0300

    [master] regex caractere especial

--- src/main/java/com/example/utility/dto/PassDto.java
+++ src/main/java/com/example/utility/dto/PassDto.java
@@ -9,5 +9,6 @@ public record PassDto(@NotNull(message = "A senha é obrigatório")
         @Pattern(regexp = ".*[A-Z].*", message = "A senha deve conter pelo menos uma letra maiúscula.")
         @Pattern(regexp = ".*[a-z].*", message = "A senha deve conter pelo menos uma letra minúscula.")
         @Pattern(regexp = ".*[0-9].*", message = "A senha deve conter pelo menos um dígito numérico.")
+        @Pattern(regexp = ".*\\W.*", message = "A senha deve conter pelo menos um caractere especial.")
                       String password) {
 }
